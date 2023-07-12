8b86c1bcce32e88a3563f6085cb2b8479406beaf
GouthamiMacherla <115568506+GouthamiMacherla@users.noreply.github.com>
2023-07-13 01:00:31 +0530

    Add files via upload

---
+++ CommonElements.java
@@ -0,0 +1,53 @@
+/*
+ * 
+ * <aside>
+💡 **Question 1**
+Given three integer arrays arr1, arr2 and arr3 **sorted** in **strictly increasing** order, return a sorted array of **only** the integers that appeared in **all** three arrays.
+
+**Example 1:**
+
+Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+
+Output: [1,5]
+
+**Explanation:** Only 1 and 5 appeared in the three arrays.
+
+</aside>
+ *
+ */
+package in.ineuron.gouthami;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonElements {
+    public static List<Integer> commonElements(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> common = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length && k < arr3.length) {
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+                common.add(arr1[i]);
+                i++;
+                j++;
+                k++;
+            } else if (arr1[i] < arr2[j]) {
+                i++;
+            } else if (arr2[j] < arr3[k]) {
+                j++;
+            } else {
+                k++;
+            }
+        }
+
+        return common;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 5, 7, 9};
+        int[] arr3 = {1, 3, 4, 5, 8};
+        List<Integer> result = commonElements(arr1, arr2, arr3);
+        System.out.println("The common elements in the three arrays are :: " +result);
+    }
+}
